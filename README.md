8148ed63512bf12b4d6315405c164d9fdb8e0612
Jonathan Peritz <jrcp123@gmail.com>
2019-05-14 09:07:44 -0700

    cleaned up code, expanded README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Kubernetes_Project2
 Teaching myself Docker and Kubernetes
+
+To use:
+1. Clone this repository
+2. Install Docker-CE, kubectl and minikube
+3. Enter the following console commands:
+minikube start
+kubectl apply -f app-service.yaml,db-service.yaml,app-deployment.yaml,db-deployment.yaml
+kubectl describe services (to get the ip address of the app endpoint)
+4. Open a web browser and connect to the ip address above, port 5000
+5. To view a json dump of the database, the address is $IP:5000/api/data
--- app/app.py
+++ app/app.py
@@ -9,7 +9,6 @@ def fav_colors() -> List[Dict]:
     config = {
         'user': 'root',
         'password': 'root',
-        #'host': 'db',
         'host': '172.17.0.3',
         'port': '3306',
         'database': 'colors'
@@ -34,8 +33,6 @@ def index():
         name = details['name']
         color = details['color']
         data_entry = (name, color)
-        #cnx = mysql.connector.connect(user='root', password='root', host='172.17.0.2', port='3306', database='colors')
-        #cnx.close()
         cnx = mysql.connector.connect(user='root', password='root', host='172.17.0.3', port='3306', database='colors')
         cursor = cnx.cursor()
         cursor.execute(add_entry, data_entry)
@@ -44,13 +41,6 @@ def index():
         cnx.close()
         return 'Success!'
     return render_template('index.html')
-
-@app.route('/chart')
-def chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"  ]
-    return render_template('chart.html', set=zip(values, labels, colors))
 
 @app.route('/api/data')
 def colors() -> str:
